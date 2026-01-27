08444363509caa479d42262b43dd458023c976b5
mattcyprinus <20032744@students.koi.edu.au>
2026-01-27 20:36:28 +1100

    Added TouristProfile

---
+++ Models/TouristProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourismPlatformMVC.Models
+{
+    public class TouristProfile
+    {
+        public int TouristId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
