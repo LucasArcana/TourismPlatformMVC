d6a8ec1aefdd9eb8890f101c23860d965f3fb5a2
Marcos Yukihiro VIEIRA YAMASHITA <20021750@students.koi.edu.au>
2026-01-29 15:38:06 +1100

    changed index heading title

---
+++ Controllers/BookingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourismPlatformMVC.Controllers
+{
+    public class BookingsController : Controller
+    {
+        // GET: Bookings
+        public ActionResult MyBookings()
+        {
+            return View();
+        }
+    }
+}
